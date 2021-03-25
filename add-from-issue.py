3321a9dde88cafa49ce839057f0ea0fc4e109621
Ivanq <imachug@yandex.ru>
2021-03-25 21:13:50 +0300

    Fix and add some more people from #837

--- add-from-issue.py
+++ add-from-issue.py
@@ -14,7 +14,7 @@ for info, content in zip(parts[::2], parts[1::2]):
 
     content_lines = [line.strip() for line in content.decode().replace("`", "").strip().split("\n")]
     for i in range(len(content_lines) - 1):
-        if content_lines[i].startswith("name:") and content_lines[i + 1].startswith("link:"):
+        if content_lines[i].lower().startswith("name:") and content_lines[i + 1].lower().startswith("link:"):
             if not os.path.isfile(f"_data/signed/{author}.yaml"):
                 with open(f"_data/signed/{author}.yaml", "w") as f:
-                    f.write(content_lines[i] + "\n" + content_lines[i + 1] + "\n")
+                    f.write("name:" + content_lines[i][5:] + "\nlink:" + content_lines[i + 1][5:] + "\n")
