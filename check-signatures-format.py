cd59544b7590cbcadc797a253b010071f923fb68
slbtty(openSUSE) <shenlebantongying@gmail.com>
2021-03-26 10:23:40 -0400

    More helpful text for the checker

--- check-signatures-format.py
+++ check-signatures-format.py
@@ -7,12 +7,12 @@ regex = re.compile(r"name: (\S+\s)*\S+\nlink: (/#|(https?://|mailto:)[a-zA-Z0-9_
 ok = True
 for file_name in sorted(os.listdir("_data/signed")):
     if not file_name.endswith(".yaml") or file_name[:-5] != file_name[:-5].strip():
-        print(file_name, "has invalid name")
+        print(file_name, "is probably a invalid name. Please wait for review or use a real name instead.")
         ok = False
     with open(f"_data/signed/{file_name}") as f:
         contents = f.read()
     if not re.fullmatch(regex, contents):
-        print(file_name, "has invalid format")
+        print(file_name, "has invalid format. Please check the format example on README and don't forget to add .yaml as file extension")
         ok = False
 
 if not ok:
