cbe2dd88300ca344df3b670588cb44355a6902dc
Ivanq <imachug@yandex.ru>
2021-04-02 16:20:56 +0300

    Import signatures with wrong format

--- add-from-issue.py
+++ add-from-issue.py
@@ -1,5 +1,5 @@
 import subprocess
-import os
+from _importer import parse_and_import_signature
 
 
 parts = subprocess.run(["gh", "issue", "view", "837", "-c"], capture_output=True).stdout.split(b"\n--\n")
@@ -11,25 +11,4 @@ for info, content in zip(parts[::2], parts[1::2]):
         info_dict[key] = value
 
     author = info_dict["author"]
-
-    content_lines = [line.strip() for line in content.decode().replace("`", "").strip().split("\n") if line.strip()]
-    imported = False
-    for i in range(len(content_lines) - 1):
-        if content_lines[i].lower().startswith("name:") and content_lines[i + 1].lower().startswith("link:"):
-            if not os.path.isfile(f"_data/signed/{author}.yaml"):
-                with open(f"_data/signed/{author}.yaml", "w") as f:
-                    f.write("name:" + content_lines[i][5:] + "\nlink:" + content_lines[i + 1][5:] + "\n")
-            imported = True
-    if not imported and len(content_lines) == 2:
-        name, link = content_lines
-        if name.lower().startswith("name:"):
-            name = name[5:]
-        name = name.strip()
-        if link.lower().startswith("link:"):
-            link = link[5:]
-        link = link.strip()
-        if "/" in link or "@" in link:
-            if not os.path.isfile(f"_data/signed/{author}.yaml"):
-                with open(f"_data/signed/{author}.yaml", "w") as f:
-                    f.write(f"name: {name}\nlink: {link}\n")
-
+    parse_and_import_signature(content.decode(), author)
--- import-from-codeberg.py
+++ import-from-codeberg.py
@@ -1,5 +1,5 @@
 import requests
-import os
+from _importer import parse_and_import_signature
 
 
 page = 1
@@ -10,26 +10,5 @@ while True:
     for comment in comments:
         author = comment["user"]["login"]
         content = comment["body"]
-
-        content_lines = [line.strip() for line in content.replace("`", "").strip().split("\n") if line.strip()]
-        imported = False
-        for i in range(len(content_lines) - 1):
-            if content_lines[i].lower().startswith("name:") and content_lines[i + 1].lower().startswith("link:"):
-                if not os.path.isfile(f"_data/signed/{author}.yaml"):
-                    with open(f"_data/signed/{author}.yaml", "w") as f:
-                        f.write("name:" + content_lines[i][5:] + "\nlink:" + content_lines[i + 1][5:] + "\n")
-                imported = True
-        if not imported and len(content_lines) == 2:
-            name, link = content_lines
-            if name.lower().startswith("name:"):
-                name = name[5:]
-            name = name.strip()
-            if link.lower().startswith("link:"):
-                link = link[5:]
-            link = link.strip()
-            if "/" in link or "@" in link:
-                if not os.path.isfile(f"_data/signed/{author}.yaml"):
-                    with open(f"_data/signed/{author}.yaml", "w") as f:
-                        f.write(f"name: {name}\nlink: {link}\n")
-
+        parse_and_import_signature(content, author)
     page += 1
